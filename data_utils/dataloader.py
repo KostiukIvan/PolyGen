021ce0f2e91bc7a53cfef8c3a61a9d98cfa53ae0
Przemyslaw Stachura <stachurap24@gmail.com>
2021-05-08 17:25:05 +0200

    tokenizer transformation + dataloader compatible with generated dataset

--- data_utils/dataloader.py
+++ data_utils/dataloader.py
@@ -4,15 +4,18 @@ import pandas as pd
 from os import listdir
 from os.path import join
 from torch.utils.data import Dataset
+from sklearn.model_selection import train_test_split
 
-categories = ['basket', 'chair', 'lamp', 'sofa', 'table']
+categories = ['02691156']
 
 
-# TODO: Implement TokenizeVertices
 class VerticesDataset(Dataset):
-    def __init__(self, root_dir=r'C:\Users\ivank\UJ\Deep learining with multiple tasks\projects\ShapeNetCore_PolyGenSubset\train',
-                 classes=['basket'], transform=[SortVertices(), NormalizeVertices(), QuantizeVertices(), ToTensor(), ResizeVertices()],
-                 split='train', train_percentage=0.925):
+    def __init__(self,
+                root_dir=r'C:\Users\ivank\UJ\Deep learining with multiple tasks\projects\ShapeNetCore_PolyGenSubset\train',
+                classes=['04330267'],
+                transform=[SortVertices(), NormalizeVertices(), QuantizeVertices(), ToTensor(), ResizeVertices(800), VertexTokenizer(801)],
+                split='train',
+                train_percentage=0.925):
         """
         Args:
             root_dir (string): Directory with all the ShapeNet data.
@@ -20,39 +23,29 @@ class VerticesDataset(Dataset):
         self.root_dir = root_dir
         self.transform = transform
         self.split = split
-
-        pc_df = self._get_names()
-        pc_df = pc_df[pc_df.category.isin(classes)].reset_index(drop=True)
-
-        self.vertices_names_train = pd.concat([pc_df[pc_df['category'] == c][
-                                                :int(train_percentage * len(pc_df[pc_df['category'] == c]))].
-                                                reset_index(drop=True) for c in classes])
-        self.vertices_names_valid = pd.concat([pc_df[pc_df['category'] == c][
-                                                int(train_percentage * len(pc_df[pc_df['category'] == c])):].
-                                                reset_index(drop=True) for c in classes])
+        
+        file_paths = []
+        for class_name in os.listdir(root_dir):
+            if not class_name in classes:
+                continue
+            for augmentation_dir in os.listdir(join(root_dir, class_name)):
+                for augmented_mesh_file in os.listdir(join(root_dir, class_name, augmentation_dir)):
+                    file_paths.append(join(root_dir, class_name, augmentation_dir, augmented_mesh_file))
+        self.X_train, self.X_valid = train_test_split(file_paths, test_size = 1 - train_percentage, shuffle=True)
+                
 
     def __len__(self):
-        if self.split == 'train':
-            pc_names = self.vertices_names_train
-        elif self.split == 'valid':
-            pc_names = self.vertices_names_valid
-        else:
+        if self.split not in {'train', 'valid'}:
             raise ValueError('Invalid split. Should be train or valid.')
-        return len(pc_names)
+        return len(self.X_train) if self.split == 'train' else len(self.X_valid)
 
     def __getitem__(self, idx):
-        if self.split == 'train':
-            pc_names = self.vertices_names_train
-        elif self.split == 'valid':
-            pc_names = self.vertices_names_valid
-        else:
+        if self.split not in {'train', 'valid'}:
             raise ValueError('Invalid split. Should be train or valid.')
 
-        pc_category, pc_filename = pc_names.iloc[idx].values
-
-        pc_filepath = join(self.root_dir, pc_category, pc_filename)
-        vertices = self._load_obj(pc_filepath)
-
+        dataset = self.X_train if self.split == 'train' else self.X_valid
+        vertices = self._load_obj(dataset[idx])
+        
         if self.transform:
             for trans in self.transform:
                 vertices = trans(vertices)
@@ -70,11 +63,10 @@ class VerticesDataset(Dataset):
     def _load_obj(self, filename):
         """Load vertices from .obj wavefront format file."""
         vertices = []
+        print(filename)
         with open(filename, 'r') as mesh:
             for line in mesh:
                 data = line.split()
                 if len(data) > 0 and data[0] == 'v':
                     vertices.append(data[1:])
         return np.array(vertices, dtype=np.float32)
-
-
