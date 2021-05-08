4996c791a28b0bd5f0cbc2305b01ac74bd3285c5
Przemyslaw Stachura <stachurap24@gmail.com>
2021-05-08 19:11:47 +0200

    sample shapes for data loading example

--- data_utils/dataloader.py
+++ data_utils/dataloader.py
@@ -6,19 +6,17 @@ from os.path import join
 from torch.utils.data import Dataset
 from sklearn.model_selection import train_test_split
 
-categories = ['02691156']
-
-
 class VerticesDataset(Dataset):
     def __init__(self,
-                root_dir=r'C:\Users\ivank\UJ\Deep learining with multiple tasks\projects\ShapeNetCore_PolyGenSubset\train',
-                classes=['04330267'],
+                root_dir,
+                classes,
                 transform=[SortVertices(), NormalizeVertices(), QuantizeVertices(), ToTensor(), ResizeVertices(800), VertexTokenizer(801)],
                 split='train',
                 train_percentage=0.925):
         """
         Args:
             root_dir (string): Directory with all the ShapeNet data.
+            classes (list): list of all data classes that can be used. Use None if all classes should be used.
         """
         self.root_dir = root_dir
         self.transform = transform
@@ -26,7 +24,7 @@ class VerticesDataset(Dataset):
         
         file_paths = []
         for class_name in os.listdir(root_dir):
-            if not class_name in classes:
+            if classes is not None and class_name not in classes:
                 continue
             for augmentation_dir in os.listdir(join(root_dir, class_name)):
                 for augmented_mesh_file in os.listdir(join(root_dir, class_name, augmentation_dir)):
@@ -51,14 +49,6 @@ class VerticesDataset(Dataset):
                 vertices = trans(vertices)
 
         return vertices
-
-    def _get_names(self) -> pd.DataFrame:
-        filenames = []
-        for category in categories:
-            for f in listdir(join(self.root_dir, category)):
-                if f not in ['.DS_Store']:
-                    filenames.append((category, f))
-        return pd.DataFrame(filenames, columns=['category', 'filename'])
 
     def _load_obj(self, filename):
         """Load vertices from .obj wavefront format file."""
--- experiments/main.py
+++ experiments/main.py
@@ -4,10 +4,11 @@ from torch.utils.data import DataLoader
 from models.VertexModel import VertexModel
 from reformer_pytorch import Reformer
 from experimetns.config import VertexConfig
+import os
 
 EPOCHS = 10
 GPU = True
-dataset_dir = r'C:\Users\ivank\UJ\Deep learining with multiple tasks\projects\ShapeNetCore_PolyGenSubset\train'
+dataset_dir = os.path.join(os.getcwd(), 'data', 'shapenet_samples')
 config = VertexPolyGenConfig(embed_dim=128, reformer__depth=6,
                              reformer__lsh_dropout=0.,
                              reformer__ff_dropout=0.,
@@ -20,12 +21,12 @@ else:
     device = None
 
 training_data = dl.VerticesDataset(root_dir=dataset_dir,
-                                   classes=['basket'],
                                    transform=[SortVertices(),
                                               NormalizeVertices(),
                                               QuantizeVertices(),
                                               ToTensor(),
-                                              ResizeVertices(500)],
+                                              ResizeVertices(800),
+                                              ResizeVertices(801)],
                                    split='train',
                                    train_percentage=0.925)
 train_dataloader = DataLoader(training_data, batch_size=2, shuffle=True)
@@ -46,4 +47,3 @@ for epoch in range(EPOCHS):
         loss.backward()
         optimizer.step()
     print(f"Epoch {epoch}: loss {total_loss}")
-
