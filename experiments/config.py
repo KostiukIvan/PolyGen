bf825d54384e17418b0ffbb89f59d6a3c66db4f9
KostiukIvan <ivan.kostiuk.if@gamil.com>
2021-05-08 16:46:04 +0200

    Basic training loop. Config file. Removed old Transformer's impl

---
+++ experiments/config.py
@@ -0,0 +1,69 @@
+class Config(object):
+    def __getitem__(self, key):
+        return self.config[key]
+
+
+class VertexConfig(Config):
+
+    def __init__(self,
+                 embed_dim=256,
+                 max_seq_len=2400,
+                 tokenizer__bos_id=0,
+                 tokenizer__eos_id=1,
+                 tokenizer__pad_id=2,
+                 embedding__vocab_value=256 + 3,
+                 embedding__vocab_coord_type=4,
+                 embedding__vocab_position=1000,
+                 embedding__pad_idx_value=2,
+                 embedding__pad_idx_coord_type=0,
+                 embedding__pad_idx_position=0,
+                 reformer__depth=12,
+                 reformer__heads=8,
+                 reformer__n_hashes=8,
+                 reformer__bucket_size=48,
+                 reformer__causal=True,
+                 reformer__lsh_dropout=0.2,
+                 reformer__ff_dropout=0.2,
+                 reformer__post_attn_dropout=0.2,
+                 reformer__ff_mult=4):
+        # tokenizer config
+        tokenizer_config = {
+            "bos_id": tokenizer__bos_id,
+            "eos_id": tokenizer__eos_id,
+            "pad_id": tokenizer__pad_id,
+            "max_seq_len": max_seq_len,
+        }
+
+        # embedding config
+        embedding_config = {
+            "vocab_value": embedding__vocab_value,
+            "vocab_coord_type": embedding__vocab_coord_type,
+            "vocab_position": embedding__vocab_position,
+            "pad_idx_value": embedding__pad_idx_value,
+            "pad_idx_coord_type": embedding__pad_idx_coord_type,
+            "pad_idx_position": embedding__pad_idx_position,
+            "embed_dim": embed_dim,
+        }
+
+        # reformer info
+        reformer_config = {
+            "dim": embed_dim,
+            "depth": reformer__depth,
+            "max_seq_len": max_seq_len,
+            "heads": reformer__heads,
+            "bucket_size": reformer__bucket_size,
+            "n_hashes": reformer__n_hashes,
+            "causal": reformer__causal,
+            "lsh_dropout": reformer__lsh_dropout,
+            "ff_dropout": reformer__ff_dropout,
+            "post_attn_dropout": reformer__post_attn_dropout,
+            "ff_mult": reformer__ff_mult,
+        }
+
+        self.config = {
+            "embed_dim": embed_dim,
+            "max_seq_len": max_seq_len,
+            "tokenizer": tokenizer_config,
+            "embedding": embedding_config,
+            "reformer": reformer_config,
+        }
--- experiments/main.py
+++ experiments/main.py
@@ -1,24 +1,49 @@
 import data_utils.dataloader as dl
 from data_utils.transformations import *
 from torch.utils.data import DataLoader
+from models.VertexModel import VertexModel
+from reformer_pytorch import Reformer
+from experimetns.config import VertexConfig
+
+EPOCHS = 10
+GPU = True
+dataset_dir = r'C:\Users\ivank\UJ\Deep learining with multiple tasks\projects\ShapeNetCore_PolyGenSubset\train'
+config = VertexPolyGenConfig(embed_dim=128, reformer__depth=6,
+                             reformer__lsh_dropout=0.,
+                             reformer__ff_dropout=0.,
+                             reformer__post_attn_dropout=0.)
 
 
-training_data = dl.VerticesDataset(root_dir=r'C:\Users\ivank\UJ\Deep learining with multiple tasks\projects\ShapeNetCore_PolyGenSubset\train',
-                classes=['basket'], transform=[SortVertices(), NormalizeVertices(), QuantizeVertices(), ToTensor(),
-                                               ResizeVertices(500)], split='train', train_percentage=0.925)
+if GPU and torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = None
 
+training_data = dl.VerticesDataset(root_dir=dataset_dir,
+                                   classes=['basket'],
+                                   transform=[SortVertices(),
+                                              NormalizeVertices(),
+                                              QuantizeVertices(),
+                                              ToTensor(),
+                                              ResizeVertices(500)],
+                                   split='train',
+                                   train_percentage=0.925)
 train_dataloader = DataLoader(training_data, batch_size=2, shuffle=True)
 
+decoder = Reformer(**config['reformer'])
+model = VertexModel(decoder, ...)
+optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
 
-for i, vertices in enumerate(train_dataloader):
-    print(i)
-
-
-
-
-
-
-
-
-
+for epoch in range(EPOCHS):
+    total_loss = 0.0
+    for i, batch in enumerate(train_dataloader):
+        model.train()
+        optimizer.zero_grad()
+        loss = model(*batch, device=device)
+        if np.isnan(loss.item()):
+            print(f"(E): Model return loss {loss.item()}")
+        total_loss += loss.item()
+        loss.backward()
+        optimizer.step()
+    print(f"Epoch {epoch}: loss {total_loss}")
 
